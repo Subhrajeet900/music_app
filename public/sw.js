3f2e68af86319d44ecd7487dc562aeae7f99a15a
harsh panchal <harshpanchal12326@gmail.com>
2026-03-31 20:32:14 +0530

    install option update

--- public/sw.js
+++ public/sw.js
@@ -2,8 +2,7 @@ const CACHE_NAME = 'moodtunes-pwa-v1';
 const STATIC_ASSETS = [
   '/',
   '/manifest.json',
-  '/icon-192x192.png',
-  '/icon-512x512.png'
+  '/app-icon.svg'
 ];
 
 self.addEventListener('install', (event) => {
